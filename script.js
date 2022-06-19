4b6aaef6ae54d1b178173ea3c4891edc0854bda2
beturba <contatobeturba@gmail.com>
2022-06-19 20:44:43 -0300

    primeiro commit

--- script.js
+++
@@ -1,9 +0,0 @@
-const nome = "Beatriz";
-const idade = 29;
-const  altura = 1.58;
-let comidaFavorita = "Pudim";
-let verdadeiro = true;
-let falso = false;
-
-console.log(`Meu nome é ${nome}, eu tenho ${idade} anos, tenho ${altura} de altura e minha comida favorita é ${comida}.`);
-console.log( `Gosto de jogar e ver series e filmes: ${verdadeiro}`)
